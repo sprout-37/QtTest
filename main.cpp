c0187c3e9b16206626056c659b2f3f0e9ddfcfba
Meng <m_zhang@sjtu.edu.cn>
2023-05-28 15:17:04 +0800

    首次提交，初始化测试界面

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // 设置字体
+    QFont font;
+    font.setFamily("Microsoft Yahei");
+    font.setPixelSize(13);
+    a.setFont(font);
+
+
+    Widget w;
+
+    w.show();
+    return a.exec();
+}
---
+++ widget.cpp
@@ -0,0 +1,114 @@
+﻿#include "widget.h"
+#include "./ui_widget.h"
+#include "flatui.h"
+#include "qdebug.h"
+#include "qdatetime.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->initForm();
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::initForm()
+{
+    ui->bar1->setRange(0, 100);
+    ui->bar2->setRange(0, 100);
+    ui->slider1->setRange(0, 100);
+    ui->slider2->setRange(0, 100);
+
+    connect(ui->slider1, SIGNAL(valueChanged(int)), ui->bar1, SLOT(setValue(int)));
+    connect(ui->slider2, SIGNAL(valueChanged(int)), ui->bar2, SLOT(setValue(int)));
+    ui->slider1->setValue(30);
+    ui->slider2->setValue(30);
+
+    this->setStyleSheet("*{outline:0px;}QWidget#frmFlatUI{background:#FFFFFF;}");
+
+    FlatUI::setPushButtonQss(ui->btn_1);
+    FlatUI::setPushButtonQss(ui->btn_2, 5, 8, "#1ABC9C", "#E6F8F5", "#2EE1C1", "#FFFFFF", "#16A086", "#A7EEE6");
+    FlatUI::setPushButtonQss(ui->btn_3, 5, 8, "#3498DB", "#FFFFFF", "#5DACE4", "#E5FEFF", "#2483C7", "#A0DAFB");
+    FlatUI::setPushButtonQss(ui->btn_4, 5, 8, "#E74C3C", "#FFFFFF", "#EC7064", "#FFF5E7", "#DC2D1A", "#F5A996");
+
+    FlatUI::setLineEditQss(ui->txt1);
+    FlatUI::setLineEditQss(ui->txt2, 5, 2, "#DCE4EC", "#1ABC9C");
+    FlatUI::setLineEditQss(ui->txt3, 3, 1, "#DCE4EC", "#3498DB");
+    FlatUI::setLineEditQss(ui->txt4, 3, 1, "#DCE4EC", "#E74C3C");
+
+    FlatUI::setProgressQss(ui->bar1);
+    FlatUI::setProgressQss(ui->bar2, 8, 5, 9, "#E8EDF2", "#1ABC9C");
+
+    FlatUI::setSliderQss(ui->slider1);
+    FlatUI::setSliderQss(ui->slider2, 10, "#E8EDF2", "#E74C3C", "#E74C3C");
+//    FlatUI::setSliderQss(ui->slider3, 10, "#E8EDF2", "#34495E", "#34495E");
+
+    FlatUI::setRadioButtonQss(ui->rbtn1);
+    FlatUI::setRadioButtonQss(ui->rbtn2, 8, "#D7DBDE", "#1ABC9C");
+    FlatUI::setRadioButtonQss(ui->rbtn3, 8, "#D7DBDE", "#3498DB");
+    FlatUI::setRadioButtonQss(ui->rbtn4, 8, "#D7DBDE", "#E74C3C");
+
+    FlatUI::setScrollBarQss(ui->horizontalScrollBar);
+    FlatUI::setScrollBarQss(ui->verticalScrollBar, 8, 120, 20, "#606060", "#34495E", "#1ABC9C", "#E74C3C");
+
+    //设置列数和列宽
+    int width = 1920;
+    ui->tableWidget->setColumnCount(5);
+    ui->tableWidget->setColumnWidth(0, width * 0.06);
+    ui->tableWidget->setColumnWidth(1, width * 0.10);
+    ui->tableWidget->setColumnWidth(2, width * 0.06);
+    ui->tableWidget->setColumnWidth(3, width * 0.10);
+    ui->tableWidget->setColumnWidth(4, width * 0.20);
+    ui->tableWidget->verticalHeader()->setDefaultSectionSize(25);
+
+    QStringList headText;
+    headText << "设备编号" << "设备名称" << "设备地址" << "告警内容" << "告警时间";
+    ui->tableWidget->setHorizontalHeaderLabels(headText);
+    ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->tableWidget->setAlternatingRowColors(true);
+    ui->tableWidget->verticalHeader()->setVisible(false);
+    ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
+
+    //设置行高
+    ui->tableWidget->setRowCount(300);
+
+    for (int i = 0; i < 300; i++) {
+        ui->tableWidget->setRowHeight(i, 24);
+        QTableWidgetItem *itemDeviceID = new QTableWidgetItem(QString::number(i + 1));
+        QTableWidgetItem *itemDeviceName = new QTableWidgetItem(QString("测试设备%1").arg(i + 1));
+        QTableWidgetItem *itemDeviceAddr = new QTableWidgetItem(QString::number(i + 1));
+        QTableWidgetItem *itemContent = new QTableWidgetItem("防区告警");
+        QTableWidgetItem *itemTime = new QTableWidgetItem(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
+
+        ui->tableWidget->setItem(i, 0, itemDeviceID);
+        ui->tableWidget->setItem(i, 1, itemDeviceName);
+        ui->tableWidget->setItem(i, 2, itemDeviceAddr);
+        ui->tableWidget->setItem(i, 3, itemContent);
+        ui->tableWidget->setItem(i, 4, itemTime);
+    }
+
+    // generate some data:
+    QVector<double> x(101), y(101); // initialize with entries 0..100
+    for (int i=0; i<101; ++i)
+    {
+        x[i] = i/50.0 - 1; // x goes from -1 to 1
+        y[i] = x[i]*x[i]; // let's plot a quadratic function
+    }
+    // create graph and assign data to it:
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(x, y);
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(-1, 1);
+    ui->customPlot->yAxis->setRange(0, 1);
+    ui->customPlot->replot();
+}
